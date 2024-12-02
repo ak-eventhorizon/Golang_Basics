9ba11a67fb33576a2b647a089e147a24dc95366d
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2024-12-02 09:19:55 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main // определение пакета для текущего файла
 import (
 	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 // ********* Net *********
@@ -8,5 +11,19 @@ import (
 // Этот пакет предоставляет различные низкоуровневые сетевые примитивы, через которые идет взамодействие по сети
 
 func main() {
-	fmt.Println("")
+	httpRequest := "GET / HTTP/1.1\n" + "Host: golang.org\n\n"
+	conn, err := net.Dial("tcp", "golang.org:80")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	if _, err = conn.Write([]byte(httpRequest)); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	io.Copy(os.Stdout, conn)
+	fmt.Println("Done")
 }
