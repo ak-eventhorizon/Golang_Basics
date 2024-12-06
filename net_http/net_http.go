62e5700afc018f66695a386e5f1a786a131a4e03
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2024-12-06 09:11:55 +0300

    Create net_http.go

---
+++ net_http/net_http.go
@@ -0,0 +1,32 @@
+package main // определение пакета для текущего файла
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// ********* net/http *********
+// все соответствующие функции по работе с http были выделены в отдельный пакет net/http
+// для отправки запросов в пакете net/http определен ряд функций: Get(), Post(), Head(), PostForm()
+
+func main() {
+	resp, err := http.Get("https://example.com")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// for {
+	// 	buffer := make([]byte, 1024)
+	// 	n, err := resp.Body.Read(buffer)
+	// 	if n == 0 || err != nil {
+	// 		break
+	// 	}
+
+	// 	fmt.Println(string(buffer[:n]))
+	// }
+
+	io.Copy(os.Stdout, resp.Body)
+}
