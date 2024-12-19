070ac63ebd1102ad34102fc539999db55bd837f7
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2024-12-19 09:10:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,34 +1,24 @@
 package main // определение пакета для текущего файла
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"time"
+	"strings"
 )
 
-// ********* net/http.Client *********
-// Для осуществления HTTP-запросов также может применяться структура http.Client
-// Структура http.Client имеет ряд полей, которые позволяют настроить ее поведение
+// ********* placeholder *********
+//
+
+func WordCount(s string) map[string]int {
+
+	var result = make(map[string]int)
+
+	words := strings.Fields(s)
+	for _, v := range words {
+		result[v] = result[v] + 1
+	}
+
+	return result
+}
 
 func main() {
-	client := &http.Client{
-		Timeout: 6 * time.Second,
-	}
-
-	req, err := http.NewRequest(
-		"GET", "https://example.com", nil,
-	)
-	// добавляем заголовки
-	req.Header.Add("Accept", "text/html")     // добавляем заголовок Accept
-	req.Header.Add("User-Agent", "MSIE/15.0") // добавляем заголовок User-Agent
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	io.Copy(os.Stdout, resp.Body)
+	fmt.Println(WordCount("one two three two four two one"))
 }
